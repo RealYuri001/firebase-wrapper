7e9c11d7927237e800d9b078d37fa72efa4fe1a2
Mr. Cat <harrowfung1016@gmail.com>
2022-07-10 12:42:59 +0800

    nab

---
+++ firebase/__init__.py
@@ -0,0 +1,25 @@
+"""
+firebase Python package is a python interface to the Google's Firebase REST APIs
+By Joe Tilsed
+"""
+from objects import Firebase, Storage, Auth, Database, FirebaseResponse, FirebaseKeyValue, KeepAuthSession, ClosableSSEClient, Stream, convert_to_firebase, convert_list_to_firebase
+
+
+
+
+name = 'firebase'
+author = "Joe Tilsed - https://linkedin.com/in/JoeTilsed"
+__all__ = (
+    'Firebase',
+    'Storage',
+    'Auth',
+    'Database',
+    'FirebaseResponse',
+    'FirebaseKeyValue',
+    'KeepAuthSession',
+    'ClosableSSEClient',
+    'Stream',
+    'convert_to_firebase',
+    'convert_list_to_firebase'
+)
+
